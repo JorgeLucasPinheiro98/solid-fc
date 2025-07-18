abe7f4d7bdf2863b648a378ee6d0a77368ebce60
JorgeLucasPinheiro98 <jorgejdn@hotmail.com>
2025-07-18 09:41:28 -0300

    Update 3.ts

--- OCP/3/3.ts
+++ OCP/3/3.ts
@@ -1,10 +1,32 @@
-class Validator {
-  validate(field: string, value: string): boolean {
-    if (field === "email") {
-      return /\S+@\S+\.\S+/.test(value);
-    } else if (field === "phone") {
-      return /^\d{10}$/.test(value);
-    }
-    return false;
+interface IValidatorStrateg{
+  validate(value: string): boolean
+}
+
+class ValidatorEmail implements IValidatorStrateg{
+  validate(value: string): boolean {
+    return /\S+@\S+\.\S+/.test(value);
   }
+}
+
+class ValidatorPhone implements IValidatorStrateg {
+  validate(value: string): boolean {
+    return /^\d{10}$/.test(value);
+  }
+}
+
+class ValidatorStrateg {
+  private strategies: Record<string, IValidatorStrateg> = {}
+
+  addstrategy(field: string, strategy: IValidatorStrateg) {
+    this.strategies[field] = strategy
+  }
+
+  validate(field: string, value: string): boolean {
+    const strategy = this.strategies[field];
+    if(!strategy) {
+      throw new Error(`No validator found for field: ${field}`);
+    }
+    return strategy.validate(value);
+  }
+
 }
